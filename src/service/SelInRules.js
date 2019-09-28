7b7e192b3d184ade8cf7363cd651851c57115d82
Javier Trejo <javier.t@geopagos.com>
2019-09-28 18:59:12 -0300

    Classes documentation
    - Added documentation for core methods

--- src/service/SelInRules.js
+++ src/service/SelInRules.js
@@ -1,8 +1,24 @@
+const Product = require('../entity/Product');
+
+/**
+ * SellIn rules manager
+ */
 class SellInRules {
+    /**
+     * Create an instance of Pricing rule set
+     *
+     * @param {Object} config
+     */
     constructor(config) {
         this.config = config;
     }
 
+    /**
+     * Resolve the sellIn factor
+     *
+     * @param {Product} product
+     * @returns {number}
+     */
     getFactor(product) {
         let factor = this.config.sellInRules.sellInFactor;
         if(product.name in this.config.specialProductsSellInRules) {
